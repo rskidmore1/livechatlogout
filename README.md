93e709322b8d4724c88194358f29f6cd720d4ee2
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2018-01-10 12:10:36 -0800

    first commit. does not include copy of crontab code

---
+++ README.md
@@ -0,0 +1,1 @@
+Automatically logs out live chat client on windows desktop at 5pm using crontab on remote server
---
+++ livechatlogout.py
@@ -0,0 +1,12 @@
+import  requests 
+from requests.auth import HTTPBasicAuth
+
+url = "https://api.livechatinc.com/agents/cesary@carmine.io"
+
+auth=HTTPBasicAuth('USERNAME', 'PASSWORD')
+headers= {'X-API-Version': '2'}
+data= {"status":"not accepting chats"}
+
+r = requests.put(url, auth=auth, headers=headers, data=data)
+
+
